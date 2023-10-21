638dc1ade030c7f4e37653a6c6f8c75784c09ca1
Mark Andrey Acebu <markandrey.acebu@gmail.com>
2023-10-21 21:36:58 +0800

    Simulated Asynchronous codes- using async, await and Future.delayed(Duration(seconds: 2), () {} });

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,29 +9,43 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
+
   @override
+  void getData() async {
+    // simulate network request for a username
+    String username = await Future.delayed(const Duration(seconds: 3), () {
+      return "Mark";
+    });
+    // simulate network request for bio
+    String bio = await Future.delayed(const Duration(seconds: 0), () {
+      return "chef, gamer";
+    });
+
+    print("$username - $bio");
+  }
+    @override
   void initState() {
-    super.initState();
-    print("initState Ran");
-  }
-  @override
-  Widget build(BuildContext context) {
-    print("Build State Ran");
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: const Text("Choose a Location"),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: ElevatedButton(onPressed: (){ setState(() {
-         counter++;
-      }); 
+      super.initState();
+        getData();
+    }
 
-      }, child: Text(" counter is $counter"),
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: const Text("Choose a Location"),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: ElevatedButton(onPressed: () {
+          setState(() {
+            counter++;
+          });
+        }, child: Text(" counter is $counter"),
 
-      ),
-    );
+        ),
+      );
+    }
   }
-}
