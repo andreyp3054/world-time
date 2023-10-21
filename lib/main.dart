6f022bd95d3cdf6f18e39285c21c9209791e13de
Mark Andrey Acebu <markandrey.acebu@gmail.com>
2023-10-21 16:54:05 +0800

    first commit - added routes, added appbar for location page

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+
+void main() => runApp(
+      MaterialApp(
+        initialRoute: "/home",    // overrides the base route
+        routes: {
+          "/": (context) => const Loading(),
+          // "/" is the base/default route, these routes takes a function argument "context" item
+          // that takes us to where in the widget tree it is
+          "/home": (context) => const Home(),
+          "/location": (context) => const ChooseLocation(),
+        },
+      ),
+    );
