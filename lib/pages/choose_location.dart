6178ca9361e9b582ff19241afafd84df72799e1a
Mark Andrey Acebu <markandrey.acebu@gmail.com>
2023-10-23 00:46:19 +0800

    added ListView.builder for selection of locations

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../services/world_time.dart';
+
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
 
@@ -8,27 +10,47 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+    WorldTime(
+        url: 'Asia/Philippines',
+        location: 'Philippines',
+        flag: 'philippines.png'),
+  ];
 
   @override
- 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          title: const Text("Choose a Location"),
-          centerTitle: true,
-          elevation: 0,
-        ),
-        body: ElevatedButton(onPressed: () {
-          setState(() {
-            counter++;
-          });
-        }, child: Text("counter is $counter"),
-
-        ),
-      );
-    }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text("Choose a Location"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+            child: Card(
+                child: ListTile(
+              onTap: () {},
+              title: Text(locations[index].location),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage("assets/${locations[index].flag}"),
+              ),
+            )),
+          );
+        },
+      ),
+    );
   }
+}
